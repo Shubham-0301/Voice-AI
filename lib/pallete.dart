2e41049f5fcbcd8527ac8950dc633735bed50a08
shubham <shubham@gmail.com>
2024-05-19 14:43:54 +0530

    first commit

---
+++ lib/pallete.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Pallete {
+  static const Color mainFontColor = Color.fromRGBO(19, 61, 95, 1);
+  static const Color firstSuggestionBoxColor = Color.fromRGBO(244, 120, 120, 1);
+  static const Color secondSuggestionBoxColor =
+      Color.fromRGBO(251, 156, 124, 1);
+  static const Color thirdSuggestionBoxColor = Color.fromRGBO(245, 170, 157, 1);
+  static const Color assistantCircleColor = Color.fromRGBO(55, 151, 219, 1);
+  static const Color borderColor = Color.fromRGBO(200, 200, 200, 1);
+  static const Color blackColor = Colors.black;
+  static const Color whiteColor = Colors.white;
+}
---
+++ lib/secrets.dart
@@ -0,0 +1,1 @@
+const openAIAPIKey = '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';
